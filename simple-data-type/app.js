d89b9e7b2aa6921f2d1211bbf895fc9cd5b21b27
kazusa-gumi <jsva4127@yahoo.co.jp>
2023-10-30 14:00:15 +1100

    コメントアウト

--- simple-data-type/app.js
+++ simple-data-type/app.js
@@ -1,7 +1,15 @@
 // Create a numerical array
-// let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
-//  // Print the array
-//  console.log(numbers); 
+let numArray = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+ // Print the array
+ console.log(numbers); 
+
+
+// Create a numerical array
+let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+// Sort the array in ascending order
+numbers.sort((a, b) => a - b);
+ // Print the array
+ console.log(numbers); 
  
 // // Remove 8 and 31 from the array
 // numbers = numbers.filter(num => num !== 8 && num !== 31); 
@@ -46,38 +54,38 @@
 
 // Accepts two arguments: an array and a target value
 // Returns the index of target value if found in the array, otherwise returns -1
-function binarySearch(array, target) {
-    // Define the left and right boundaries of the search
-    let left = 0;
-    let right = array.length - 1;
+// function binarySearch(array, target) {
+//     // Define the left and right boundaries of the search
+//     let left = 0;
+//     let right = array.length - 1;
   
-    // Continue searching as long as there is a valid subarray to search within
-    while (left <= right) {
-      // Calculate the mid index
-      let mid = Math.floor((left + right) / 2);
+//     // Continue searching as long as there is a valid subarray to search within
+//     while (left <= right) {
+//       // Calculate the mid index
+//       let mid = Math.floor((left + right) / 2);
   
-      // If the middle element is the target, return its index
-      if (array[mid] === target) {
-        return mid;
-      } 
-      // If the middle element is less than the target, discard the left half
-      else if (array[mid] < target) {
-        left = mid + 1;
-      } 
-      // If the middle element is greater than the target, discard the right half
-      else {
-        right = mid - 1;
-      }
-    }
+//       // If the middle element is the target, return its index
+//       if (array[mid] === target) {
+//         return mid;
+//       } 
+//       // If the middle element is less than the target, discard the left half
+//       else if (array[mid] < target) {
+//         left = mid + 1;
+//       } 
+//       // If the middle element is greater than the target, discard the right half
+//       else {
+//         right = mid - 1;
+//       }
+//     }
   
-    // If the search completes without finding the target, return -1
-    return -1;
-  }
+//     // If the search completes without finding the target, return -1
+//     return -1;
+//   }
   
-  // Array of numbers to search in 
-  let numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
+//   // Array of numbers to search in 
+//   let numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
   
   
-  // Test 2: Search for target 100 in the array. 
-  // Since 100 is not present in the array, -1 will be printed 
-  console.log(binarySearch(numbers, 100));
+//   // Test 2: Search for target 100 in the array. 
+//   // Since 100 is not present in the array, -1 will be printed 
+//   console.log(binarySearch(numbers, 100));
